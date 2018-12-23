355b019e7c2a2639d008f68c481427325afa0baf
bsdbandit <44208456+bsdbandit@users.noreply.github.com>
2018-12-23 13:56:58 -0500

    Add files via upload
    This script will work with both python version 2 & 3 
    you will need to install the pexpect module which can by using pip 
    
    pip install pexpect
    pip3 install pexpect

---
+++ system_update.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+#################################################
+#This script keeps my mac osx uptodate with the #    
+#latest patches and updates                     #
+#written by bsdbandit                           #
+#sept 30, 2010                                  #
+#################################################
+
+from subprocess import call, PIPE, Popen
+import pexpect 
+import getpass
+import sys
+
+################################
+#This function applies updates #
+#to your Operating System      #
+#################################
+
+
+def software_update():
+            p = getpass.getpass()
+            child = pexpect.spawn('sudo softwareupdate -d -i -r')
+            child.expect('Password:')
+            child.sendline(p)
+            child.expect('$')
+            child.interact()
+
+
+
+
+
+
+software_update()
+
+
+
+
+
+
