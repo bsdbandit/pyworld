05a399e4f206be5ed3fee9faa0a704342bfafff8
bsdbandit <44208456+bsdbandit@users.noreply.github.com>
2018-12-23 14:02:00 -0500

    Update system_update.py

--- system_update.py
+++ system_update.py
@@ -3,7 +3,11 @@
 #This script keeps my mac osx uptodate with the #    
 #latest patches and updates                     #
 #written by bsdbandit                           #
+#pexpect module will need to be installed in    #
+#for this script to run in either python 2 or 3 #
 #################################################
+
+
 
 from subprocess import call, PIPE, Popen
 import pexpect 
